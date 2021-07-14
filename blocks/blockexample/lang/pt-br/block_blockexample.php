10474e5344deb7c6ed81fae227cddd197b0911d6
Douglas-Castro <farias.douglascastro@gmail.com>
2021-07-14 18:38:17 -0300

    :bulb: Add block example

---
+++ blocks/blockexample/lang/pt-br/block_blockexample.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Bloco HTML simples';
+$string['blockexample'] = 'HTML Simples';
+$string['blockexample:addinstance'] = 'Adicione um novo Bloco HTML simples';
+$string['blockexample:myaddinstance'] = 'Adicione um bloco de HTML simples para a página Meu Moodle';
