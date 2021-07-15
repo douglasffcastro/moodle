385c64d51a3f4c1707c37f946cf9f13433811196
Douglas-Castro <farias.douglascastro@gmail.com>
2021-07-15 20:00:14 -0300

    :arrow_up: Change directory name

---
+++ blocks/blockexample/lang/en/block_blockexample.php
@@ -0,0 +1,6 @@
+<?php
+$string['pluginname'] = 'HTML Block Example';
+$string['blockexample'] = 'Hi, I am title';
+$string['blockexample:addinstance'] = 'Add a new simple HTML block';
+$string['blockexample:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page';
+$string['blockstring']= 'O que isso faz?';
--- blocks/blockexample/lang/pt-br/block_blockexample.php
+++
@@ -1,5 +0,0 @@
-<?php
-$string['pluginname'] = 'Bloco HTML simples';
-$string['blockexample'] = 'HTML Simples';
-$string['blockexample:addinstance'] = 'Adicione um novo Bloco HTML simples';
-$string['blockexample:myaddinstance'] = 'Adicione um bloco de HTML simples para a página Meu Moodle';
