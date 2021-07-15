a3772ec9727c2ca382d0e84da8bbed406b5eb45b
Douglas-Castro <farias.douglascastro@gmail.com>
2021-07-15 20:01:13 -0300

    :bulb: Add multiple instance function

--- blocks/blockexample/block_blockexample.php
+++ blocks/blockexample/block_blockexample.php
@@ -3,15 +3,38 @@ class block_blockexample extends block_base {
     public function init() {
         $this->title = get_string('blockexample', 'block_blockexample');
     }
+
     public function get_content() {
         if ($this->content !== null) {
           return $this->content;
         }
     
         $this->content         =  new stdClass;
-        $this->content->text   = 'Here is the content of block!';
-        $this->content->footer = 'The footer is here.';
+        $this->content->text   = 'Tudo que estiver aqui está no body do bloco';
+        $this->content->footer = 'E aqui tudo que for o footer do bloco.';
      
         return $this->content;
-    }   
+    }
+
+    public function specialization() {
+        if (isset($this->config)) {
+            if (empty($this->config->title)) {
+                $this->title = get_string('defaulttitle', 'block_blockexample');            
+            } else {
+                $this->title = $this->config->title;
+            }
+    
+            if (empty($this->config->text)) {
+                $this->config->text = get_string('defaulttext', 'block_blockexample');
+            }    
+        }
+    }
+
+    function instance_allow_multiple() {
+        return true;
+    }
+
+    function has_config() {
+        return true;
+    }
 }
