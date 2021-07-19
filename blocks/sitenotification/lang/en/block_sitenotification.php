95157d1f86bf42ec920266255a2144e2d354e44e
Douglas-Castro <farias.douglascastro@gmail.com>
2021-07-19 15:36:49 -0300

    :bulb: Add: strings for block Site Notification

---
+++ blocks/sitenotification/lang/en/block_sitenotification.php
@@ -0,0 +1,13 @@
+<?php
+$string['pluginname']             = 'Site Notification Block';
+$string['generalheading']         = 'Site notification settings';
+$string['shownotification']       = 'Show the Notification block';
+$string['blockheading']           = 'Administration setup for Site Notification';
+
+// Admin settings
+$string['adminpageheading']       = 'Site notification settings';
+$string['notificationmessage']    = 'Type the message';
+$string['notificationcolor']      = 'Notification alert color';
+$string['notificationcolor_desc'] = 'Select a color in which the notification will be displayed';$string['simplehtml'] = 'Simple HTML';
+$string['simplehtml:addinstance'] = 'Add a new simple HTML block';
+$string['simplehtml:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page';
